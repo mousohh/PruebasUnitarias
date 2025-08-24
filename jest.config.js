088d1281e80cf4815fc7fec8ec0f2fccc07d4fc5
mousohh <165579014+mousohh@users.noreply.github.com>
2025-08-23 22:27:24 -0500

    Remove coverageThreshold configuration from Jest settings

--- jest.config.js
+++ jest.config.js
@@ -22,16 +22,6 @@ module.exports = {
   // Archivos a incluir en la cobertura
   collectCoverageFrom: ["src/**/*.js", "!src/config/**", "!src/server.js", "!**/node_modules/**"],
 
-  // Umbrales de cobertura
-  coverageThreshold: {
-    global: {
-      branches: 70,
-      functions: 70,
-      lines: 70,
-      statements: 70,
-    },
-  },
-
   // Limpiar mocks automáticamente
   clearMocks: true,
 
